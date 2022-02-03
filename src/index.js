2de7d48a210b0d311ee82732de7a5df957fa9bd0
Tochukwu <tochukwuodoeme@github.com>
2022-02-03 11:48:24 +0100

    Firestore setup and fetching db collection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 const firebaseConfig = {
     apiKey: "AIzaSyCL7Nds1VwhyzwPxETlQle2rDB-6Rz_awI",
     authDomain: "fir-9-intro-617b2.firebaseapp.com",
@@ -8,4 +11,25 @@ const firebaseConfig = {
     appId: "1:315580407129:web:667e21f92d57219e95c048"
   };
 
+  //init firebase app
   initializeApp(firebaseConfig)
+
+  //init firestore service
+  const db = getFirestore()
+
+  //collection ref - get a specific sollection
+  const colRef = collection(db, 'books')
+
+  //retrieve collection data
+  getDocs(colRef)
+    .then((snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) =>{
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    })
+
+    .catch(err => {
+        console.log(err.message);
+    })
