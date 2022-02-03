9a0bd8a1529d481d0bc00010a3b9869af34881c7
Tochukwu <tochukwuodoeme@github.com>
2022-02-03 11:25:38 +0100

    firebase installation and setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello dear")
+import { initializeApp } from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyCL7Nds1VwhyzwPxETlQle2rDB-6Rz_awI",
+    authDomain: "fir-9-intro-617b2.firebaseapp.com",
+    projectId: "fir-9-intro-617b2",
+    storageBucket: "fir-9-intro-617b2.appspot.com",
+    messagingSenderId: "315580407129",
+    appId: "1:315580407129:web:667e21f92d57219e95c048"
+  };
+
+  initializeApp(firebaseConfig)
